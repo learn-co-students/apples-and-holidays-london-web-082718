b0376b0fe8d8908896aede39a36a2184ebcdca5e
Bhagya Raj <bhagyaraj01@gmail.com>
2018-08-22 16:00:03 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,9 +83,9 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, item|
-      holiday if item.include?("BBQ")
+      holiday if item.include?("BBQ")      # THIS IS NEAT - it sorts out holidays
     end 
-  end.flatten.compact
+  end.flatten.compact           # IMPLICITLY FLATTENS the HOLIDAYS
 end
 
 
